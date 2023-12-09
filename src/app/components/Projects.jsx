9014b60f77160318da37fd4f4dc38dd717e9997b
Mauro <sinopolimauro@gmail.com>
2023-12-09 14:37:28 -0300

    add news proyects

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -19,7 +19,7 @@ const projectsData = [
     description:
       'Shop es una tienda en línea desarrollada en React, utilizando tecnologías como Firebase para funciones en la nube, React Bootstrap para la interfaz, React Router para la navegación, y SweetAlert2 para ventanas modales. Además, se emplean herramientas de testing como Testing Library. Puedes explorar la tienda en github.',
     gitUrl: 'https://github.com/sinomau/ReactShop/tree/main',
-    liveUrl: 'https://sinomau.github.io/ReactShop/',
+    liveUrl: 'https://react-shop-iota-nine.vercel.app/',
   },
   {
     id: 3,
@@ -30,6 +30,23 @@ const projectsData = [
     gitUrl: 'https://github.com/sinomau/Backend/tree/main',
     liveUrl: 'https://backend-production-ac1d.up.railway.app/',
   },
+  {
+    id: 4,
+    imgUrl: 'images/projects/desarrollo.png',
+    title: 'Desarrollo Web HTML - CSS',
+    description:'Este proyecto no solo representó un desafío emocionante sino también una oportunidad para consolidar mis habilidades en el desarrollo web. Cada fase del proyecto fue respaldada por la sólida base teórica y práctica proporcionada por el curso, permitiéndome construir un portafolio web sólido y funcional. ¡Estoy emocionado de compartir este viaje de aprendizaje y desarrollo con ustedes!',
+    gitUrl: 'https://github.com/sinomau/Proyecto-Desarrollo-Web',
+    liveUrl: 'https://sinomau.github.io/Proyecto-Desarrollo-Web/',
+  },
+  {
+    id:4,
+    imgUrl: 'images/projects/vanilla.png',
+    title: 'Javascript vanilla Ecommerce',
+    description:'Este proyecto no solo me desafió a aplicar mis conocimientos de JavaScript en un entorno práctico, sino que también me proporcionó una comprensión profunda de los aspectos avanzados de este poderoso lenguaje de programación. Cada etapa del curso fue fundamental para el desarrollo exitoso de este proyecto, consolidando mi habilidad para crear soluciones interactivas y dinámicas utilizando JavaScript',
+    gitUrl: 'https://github.com/sinomau/Proyecto-Final-JS',
+    liveUrl: 'https://sinomau.github.io/Proyecto-Final-JS/', 
+  },
+
 ];
 
 const Projects = () => {
